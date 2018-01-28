0e6fca5dee294d0fbe275798959604e5e5704766
AnselmeClergeot <anselmedu25@hotmail.com>
2018-01-28 14:20:55 +0100

    First working version

--- skeleton.py
+++ skeleton.py
@@ -1,72 +1,107 @@
 #-*- coding: utf-8 -*-
 
-from colorama import init, Fore, Back, Style
-import sys, os
+import os, sys
+from colorama import init, Fore, Style
 
-init()
 
-def getPath(fileName) :
-    return os.path.join(sys.path[0], fileName)
-
-def sayHello() :
-    with open(getPath('logo'), 'r') as logo_file :
-        logo = logo_file.read()
-
-        print(Style.BRIGHT + Fore.CYAN + logo)
-        print(Fore.GREEN + "Simple file skeleton generator 1.0.0\n")
-
-def askFile() :
-    fullName = raw_input(Style.RESET_ALL + Style.BRIGHT + Fore.BLUE + "Enter the name of your file with extension : " + Style.RESET_ALL)
-
-    if len(fullName) == 0 :
-        print(Style.BRIGHT + Fore.RED + "You entered nothing.")
-        return -1
-
-    parsed = fullName.split('.')
-
-    if len(parsed) == 1 :
-        print(Style.BRIGHT + Fore.RED + "Your file has no extension.")
-        return -1
-
-    name, extension = parsed[0], parsed[1]
-
-    if len(name) == 0 :
-        print(Style.BRIGHT + Fore.RED + "Your file has no name.")
-        return -1
-        
-    if len(extension) == 0 :
-        print(Style.BRIGHT + Fore.RED + "Your file hs no extension.")
-        return -1
-
-    return name, extension
-
-def getFileType(fileName) :
-
-    return fileName[fileName.index('.')+1:]
-
-def writeFile(fileName, content) :
-    try :
-        newFile = open(os.path.join(os.getcwd(), fileName), 'w')
-        newFile.write(content)
-    except :
-        print(Style.BRIGHT + Fore.RED + "Cannot create file. Try running skeleton with sudo or check filename.")
-        return
-
-    print(Style.BRIGHT + Fore.GREEN + "File created.")
-
-def chooseTemplate(fileType) :
-    path = getPath(os.path.join('templates', fileType))
-
-    if not os.path.exists(path) :
-        print(Style.BRIGHT + Fore.RED + "No templates were found for .{} files.".format(fileType))
+def fileInfos(fileName) :
+    base = os.path.splitext(os.path.basename(fileName))
+    name, extension = base[0], base[1][1:]
+    
+    if len(name) != 0 and len(extension) != 0 :
+        return base, extension
 
     else :
-        templatesNb = len([f for f in os.listdir(path)])
-        print(Style.BRIGHT + Fore.GREEN + "{} template(s) existing for .{} files.".format(templatesNb, fileType))
+        return -1
+
+def templateExists(extension) :
+    templatesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates', extension)
+
+    return os.path.exists(templatesPath) and len(os.listdir(templatesPath)) > 0
+
+def displayTemplates(extension) :
     
+    templatesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates', extension)
+
+    templateNum = len(os.listdir(templatesPath))
+
+    for i in range(templateNum) :
+        with open(os.path.join(templatesPath, str(i))) as template :
+            desc = template.readline().strip()
+            print(Fore.CYAN + "{}) ".format(i) + desc)
+
+    print('')
+    return templateNum
+
+def choseTemplate(extension) :
+
+    templateNum = displayTemplates(extension)
+    choiceCorrect = False
+
+    while not choiceCorrect :
+        choiceCorrect = True
+
+        try :
+            choice = int(raw_input(Fore.CYAN + "Chose : "))
+
+            if not (0 <= choice < templateNum) :
+                print(Fore.RED + "Please chose an existing template.")
+                choiceCorrect = False
+
+        except ValueError :
+            print(Fore.RED + "Please unter a number.")
+            choiceCorrect = False
+
+    return choice
+
+def canWrite(path) :
+    return not os.path.exists(path)
+
+def writeFile(path, template) :
+     
+    templatesPath = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'templates', extension)
+
+    with open(os.path.join(templatesPath, str(template)), 'r') as template :
+        content = '\n'.join(template.read().split('\n')[1:])
+        
+        with open(path, 'w') as newFile :
+            newFile.write(content)
+        
 if __name__ == '__main__' :
 
-    sayHello()
-    results = askFile()
+    init() #For colorama
 
-    if results != -1 :
+    params = sys.argv[1:]
+
+    if len(params) == 0 :
+        print(Fore.RED + "Error, incorrect number of parameters.")
+        sys.exit(-1)
+
+    fileName = params[0]
+
+    results = fileInfos(fileName)
+
+    if results == -1 :
+        print(Fore.RED + "Error, incorrect file name.")
+        sys.exit(-1)
+
+    name, extension = results[0], results[1]
+
+    if not canWrite(fileName) :
+        print(Fore.RED + "Will not be able to create file {}, file already exists.".format(fileName))
+        sys.exit(-1)
+
+
+    if not templateExists(extension) :
+        print(Fore.RED + "No template found for " + Style.BRIGHT + ".{}".format(extension) + Style.RESET_ALL + Fore.RED + " files.")
+        sys.exit(-1)
+
+
+    template = 0
+
+    if '-t' in params :
+        template = choseTemplate(extension)
+
+    writeFile(fileName, template)
+    print(Fore.GREEN + "File was successfully created.")
+
